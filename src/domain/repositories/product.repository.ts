949c58f534628b349a677b038fc04a2163851727
Wallas Faria <wallasfaria@hotmail.com>
2021-07-09 00:01:29 -0300

    add comment

--- src/domain/repositories/product.repository.ts
+++ src/domain/repositories/product.repository.ts
@@ -3,6 +3,7 @@ import { Product, ProductCollectionItem } from '../models/product.model'
 import { RelatedProduct } from '../models/related-product.model'
 
 export interface ProductRepositoty {
+  // TODO: getProducts deverá permitir receber parametros de paginação e filtro
   getProducts(): Promise<Collection<ProductCollectionItem>>
   getProduct(id: number): Promise<Product>
   updateProduct(id: number, attributes: Record<string, any>): Promise<Product>
